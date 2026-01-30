200d89797ea20485c48079a6a82ec28692e78d07
Youssef Amr <youssefahmedamrgk@gmail.com>
2026-01-31 00:26:48 +0400

    2xl size for loading

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function loading() {
   return (
         <div className="flex justify-center align-items-center">
-            <span className="loading loading-bars loading-xl !bg-gradient-to-r !from-pink-400 !to-blue-400 scale-120 "></span>
+            <span className="loading loading-bars loading-xl !bg-gradient-to-r !from-pink-400 !to-blue-400 scale-120 2xl:scale-150 "></span>
 
         </div>
     )
